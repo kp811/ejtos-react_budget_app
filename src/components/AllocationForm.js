074ddb2232c58b06c3ce0de698b61341e19b4d7e
kp811 <150534213+kp811@users.noreply.github.com>
2023-11-24 16:31:20 +0100

    fixed budget not updating + ui change

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -57,7 +57,7 @@ const AllocationForm = (props) => {
                   </div>
                   <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
                         <option defaultValue value="Add" name="Add">Add</option>
-                <option value="Reduce" name="Reduce">Reduce</option>
+                        <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
 
                     <input
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,14 +2,19 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: event.target.value,    //newBudget updates after function
+        })
+        
     }
     return (
         <div className='alert alert-secondary'>
-        <span>Budget: £{budget}</span>
+        <span>Budget: £</span>
         <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
         </div>
     );
